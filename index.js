a26f99cdaa6e9e5c601b8041191b92e56b499a7b
Aakib javed <ajaakib20@gmail.com>
2023-11-01 19:19:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const usernameInput = document.getElementById("username-input");
+//     const usernameLabel = document.getElementById("username-label");
+
+//     usernameInput.addEventListener("focus", function() {
+//         usernameLabel.classList.add("active");
+//     });
+
+//     usernameInput.addEventListener("blur", function() {
+//         if (usernameInput.value === "") {
+//             usernameLabel.classList.remove("active");
+//         }
+//     });
+// });
+
